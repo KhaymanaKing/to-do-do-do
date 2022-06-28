5a59578a78b82d65e0ba1fa0c9b3a573d9f8b56f
Khayman King <khaymanak@gmail.com>
2022-06-28 14:36:37 -0700

    finished create to-do test

---
+++ __tests__/todo.test.js
@@ -0,0 +1,43 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+  email: 'wow@test.com',
+  password: '123456789'
+};
+
+// const mockUser2 = {
+//   email: 'wow@test.com',
+//   password: '123456789'
+// };
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...userProps });
+  const { email } = user;
+  await agent
+    .post('/api/v1/users/sessions')
+    .send({ email, password });
+  return [agent, user];
+};
+
+describe('todos', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('creates a new to do for current user', async() => {
+    const [agent] = await registerAndLogin();
+    const newTodo = { todo: 'Yell' };
+    const res = await agent
+      .post('/api/v1/to-dos')
+      .send(newTodo);
+    expect(res.status).toEqual(200);
+  });
+
+});
+
+
