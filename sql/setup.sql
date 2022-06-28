ee4c53e037f02e5b8dbf063bf8fdb1bfe999893b
Khayman King <khaymanak@gmail.com>
2022-06-28 14:25:06 -0700

    todo table built and files added accordingly

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,18 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users CASCADE;
-
+DROP TABLE IF EXISTS todos;
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email VARCHAR NOT NULL UNIQUE,
   password_hash VARCHAR NOT NULL
 );
+
+CREATE TABLE todos (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT,
+  todo VARCHAR NOT NULL,
+  finished BOOLEAN NOT NULL DEFAULT(false),
+  created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  FORIEGN KEY (user_id) REFERENCES users(id)
+)
